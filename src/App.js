c7ce77bc0c5e021f5384118d830842960595d0df
ASM <Alexanderstubmichelsen@gmail.com>
2023-01-24 11:10:04 +0100

    Privacy policy added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
-ImageOne from '../../assets/images/PrivacyPolicy/1.jpg'; // gives image path
-import ImageTwo from '../../assets/images/PrivacyPolicy/2.jpg'; // gives image path
-import ImageThree from '../../assets/images/PrivacyPolicy/3.jpg'; // gives image path
-import ImageFour from '../../assets/images/PrivacyPolicy/4.jpg'; // gives image path
-import ImageFive from '../../assets/images/PrivacyPolicy/5.jpg'; // gives image path
-import ImageSix from '../../assets/images/PrivacyPolicy/6.jpg'; // gives image path
-import ImageSeven from '../../assets/images/PrivacyPolicy/7.jpg'; // gives image path
-import ImageEight from '../../assets/images/PrivacyPolicy/8.jpg'; // gives image path
-import ImageNine from '../../assets/images/PrivacyPolicy/9.jpg'; // gives image path
-import ImageTen from '../../assets/images/PrivacyPolicy/10.jpg'; // gives image path
-import ImageEleven from '../../assets/images/PrivacyPolicy/11.jpg'; // gives image path
-import ImageTwelve from '../../assets/images/PrivacyPolicy/12.jpg'; // gives image path
+import PrivacyPolicy1 from './assets/images/PrivacyPolicy/1.jpg'
+import PrivacyPolicy2 from './assets/images/PrivacyPolicy/2.jpg'
+import PrivacyPolicy3 from './assets/images/PrivacyPolicy/3.jpg'
+import PrivacyPolicy4 from './assets/images/PrivacyPolicy/4.jpg'
+import PrivacyPolicy5 from './assets/images/PrivacyPolicy/5.jpg'
+import PrivacyPolicy6 from './assets/images/PrivacyPolicy/6.jpg'
+import PrivacyPolicy7 from './assets/images/PrivacyPolicy/7.jpg'
+import PrivacyPolicy8 from './assets/images/PrivacyPolicy/8.jpg'
+import PrivacyPolicy9 from './assets/images/PrivacyPolicy/9.jpg'
+import PrivacyPolicy10 from './assets/images/PrivacyPolicy/10.jpg'
+import PrivacyPolicy11 from './assets/images/PrivacyPolicy/11.jpg'
+import PrivacyPolicy12 from './assets/images/PrivacyPolicy/12.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy1" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy2" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy3" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy4" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy5" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy6" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy7" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy8" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy9" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy10" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy11" />
+        <img src={PrivacyPolicy1} alt="PrivacyPolicy12" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

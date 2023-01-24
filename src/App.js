0f80ce56f3622aa2bbce87f768eba533b64d7b2c
ASM <Alexanderstubmichelsen@gmail.com>
2023-01-24 10:50:57 +0100

    Images of privacy policy added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+ImageOne from '../../assets/images/PrivacyPolicy/1.jpg'; // gives image path
+import ImageTwo from '../../assets/images/PrivacyPolicy/2.jpg'; // gives image path
+import ImageThree from '../../assets/images/PrivacyPolicy/3.jpg'; // gives image path
+import ImageFour from '../../assets/images/PrivacyPolicy/4.jpg'; // gives image path
+import ImageFive from '../../assets/images/PrivacyPolicy/5.jpg'; // gives image path
+import ImageSix from '../../assets/images/PrivacyPolicy/6.jpg'; // gives image path
+import ImageSeven from '../../assets/images/PrivacyPolicy/7.jpg'; // gives image path
+import ImageEight from '../../assets/images/PrivacyPolicy/8.jpg'; // gives image path
+import ImageNine from '../../assets/images/PrivacyPolicy/9.jpg'; // gives image path
+import ImageTen from '../../assets/images/PrivacyPolicy/10.jpg'; // gives image path
+import ImageEleven from '../../assets/images/PrivacyPolicy/11.jpg'; // gives image path
+import ImageTwelve from '../../assets/images/PrivacyPolicy/12.jpg'; // gives image path
+
 function App() {
   return (
     <div className="App">

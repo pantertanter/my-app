cd0d225e7644d6a694c52d8563b65eb41b27c12f
ASM <Alexanderstubmichelsen@gmail.com>
2023-02-21 21:14:21 +0100

    Contact site enabled with credentials
    and a link to my LinkediN PROFILE.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Picture from './components/Picture';
-import CatFacts from './Pages/FrontPage/Cat-Facts'
+import CatFacts from './Pages/Cat-Facts'
 import ImageRnd from './components/ImageRnd'
 import Navbar from './components/Navbar';
 import { Picture2 } from './components/Picture';
--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import '../CssFolder/PrivacyPolicy.css';
+import '../CssFolder/ContactPage.css';
 import Navbar from '../components/Navbar';
+import ContactImg from '../assets/images/Contact/contact-us-graphic.jpg'
+import PacPic from '../assets/images/Contact/pacman_PNG66.png'
+
+let source = "https://www.linkedin.com/in/alexander-stub-michelsen-2a6a301a4/"
+
 
 export default function ContactPage() {
   return (
@@ -9,7 +14,31 @@ export default function ContactPage() {
       <header>
         <Navbar />
         </header>
-        <div>ContactPage</div>
+        <div className='ContactInfo'>
+          <ul className='ContactInfo-Schema'>
+              <li>
+              <h4>Alexander Stub Michelsen</h4>
+              </li>
+              <li>
+              <h4>Mathilde Parken 23, 2. MF.</h4>
+              </li>
+              <li>
+              <h4>3400 Hillerød, DK</h4>
+              </li>
+              <li>
+              <h4>AlexanderStubMichelsen@gmail.com</h4>
+              </li>
+              <li>
+              <h4>004528919844</h4>
+              </li>
+              <li>
+              <a href={source}><p>LinkedIn</p></a>
+              </li>
+
+          </ul>
+          <img src={ContactImg} alt="ContactImg" loading=" lazy" /> 
+          <img src={PacPic} alt="ContactImg" loading=" lazy" />                
+        </div>
         </div>
   )
 }
